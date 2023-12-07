ed55b481e1a82d97c1f3fd8c6ed921d93eb24a6c
jean0313 <denggang80@gmail.com>
2023-12-07 15:45:20 +0800

    chore: build

--- main.go
+++ main.go
@@ -4,11 +4,15 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
+	"fmt"
 	"k2a/cmd"
 	"k2a/internal/k2a"
 )
 
+var Version = "0.0.1"
+
 func main() {
+	fmt.Printf("version: %v\n", Version)
 	cmd.Execute()
 }
 
