18fae5191de14dfa0395f234055ce25f71cda7b7
jean0313 <denggang80@gmail.com>
2023-12-08 11:43:56 +0800

    doc: update

--- cmd/k2a.go
+++ cmd/k2a.go
@@ -31,7 +31,7 @@ func init() {
 	k2aCmd.Flags().StringVar(&config.SchemaRegistryUrl, "rurl", "http://localhost:8081", "Schema registry url")
 	k2aCmd.Flags().StringVar(&config.Topics, "topics", "", "Topics to export")
 	k2aCmd.Flags().StringVar(&config.File, "file", "k2a.yaml", "Output file name")
-	k2aCmd.Flags().StringVar(&config.SpecVersion, "spec-version", "1.0.0", "Version number of the output file. (default 1.0.0)")
+	k2aCmd.Flags().StringVar(&config.SpecVersion, "spec-version", "1.0.0", "Version number of the output file.")
 	k2aCmd.Flags().StringVar(&config.Certificate, "cert", "", "The optional certificate file for client authentication")
 	k2aCmd.Flags().StringVar(&config.KeyFile, "key-file", "", "The optional key file for client authentication")
 	k2aCmd.Flags().StringVar(&config.CAFile, "ca-file", "", "The optional certificate authority file for TLS client authentication")
