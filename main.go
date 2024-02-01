6f95b2783da317cdc05f1d0d6d282f8d971573ea
jean0313 <denggang80@gmail.com>
2024-02-01 18:04:39 +0800

    doc: update

--- main.go
+++ main.go
@@ -6,14 +6,12 @@ package main
 import (
 	"k2a/cmd"
 	"k2a/internal/k2a"
-
-	"go.uber.org/zap"
 )
 
 var Version = "0.0.1"
 
 func main() {
-	zap.L().Info("start", zap.String("version", Version))
+	// zap.L().Info("start", zap.String("version", Version))
 	cmd.Execute()
 }
 
